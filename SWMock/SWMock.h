e5d1e599b6028cfff9b4e3ac4a45034aa96aafd0
Łukasz Przytuła <lprzytula@gmail.com>
2019-03-03 20:11:46 +0100

    Initial Commit

---
+++ SWMock/SWMock.h
@@ -0,0 +1,19 @@
+//
+//  SWMock.h
+//  SWMock
+//
+//  Created by Łukasz Przytuła on 03/03/2019.
+//  Copyright © 2019 Mildware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWMock.
+FOUNDATION_EXPORT double SWMockVersionNumber;
+
+//! Project version string for SWMock.
+FOUNDATION_EXPORT const unsigned char SWMockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWMock/PublicHeader.h>
+
+
